5dbfec9ad5a2a1129fa76162f869192c71e718b7
vagrant <vagrant@homestead>
2016-11-20 08:32:03 +0000

    111

--- src/Application.php
+++ src/Application.php
@@ -1,10 +1,23 @@
 <?php
 
-namespace Tgm\Strat;
+namespace Tgm\Start;
 
-class Application {
+use Laravel\Lumen\Application as App;
 
-    public function index() {
+class Application extends App{
+
+        /**
+     * Create a new Lumen application instance.
+     *
+     * @param  string|null  $basePath
+     * @return void
+     */
+    public function __construct($basePath = null)
+    {
+        parent::__construct($basePath = null);
+    }
+    
+    public function tgm() {
         echo "ddddaaadd test success";
     }
 
